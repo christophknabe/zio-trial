e8d4bf3db9ca37979cdea54fa3c4a893690f9f8b
Christoph Knabe <knabe@beuth-hochschule.de>
2021-03-05 12:22:50 +0100

    toLong(String) returns ZIO, reduce Seq of ZIOs.

--- src/test/scala/zio_trial/ZioExampleSpec.scala
+++ src/test/scala/zio_trial/ZioExampleSpec.scala
@@ -14,6 +14,7 @@ object ZioExampleSpec extends DefaultRunnableSpec {
 
   def print(msg: String): UIO[Unit] = ZIO.effectTotal(println(msg))
   def multiply(x: Int, y: Int): UIO[Int] = ZIO.effectTotal(x * y)
+  def toLong(string: String): Task[Long] = Task.effect(string.toLong)
 
   def spec =
     suite(MethodHandles.lookup.lookupClass.getSimpleName.stripSuffix("$"))(
@@ -45,6 +46,26 @@ object ZioExampleSpec extends DefaultRunnableSpec {
           _ <- print(s"2*3=$product")
         } yield assert(product)(equalTo(6))
         testResult
+      },
+      testM("toLong success") {
+        val zioResult: Task[Long] = toLong("123")
+        val testResult = zioResult.map(n => assert(n)(equalTo(123L)))
+        testResult
+      },
+      testM("toLong with unparsable argument") {
+        //when
+        val zioResult: Task[Long] = toLong("12A3")
+        val testResult = zioResult.flip.map { th =>
+          assert(th)(isSubtype[NumberFormatException](anything)) &&
+          assert(th.getMessage)(equalTo("""For input string: "12A3""""))
+        }
+        testResult
+      },
+      testM("reduce a Seq of ZIOs") {
+        val numberStrings = (1 to 10).map(_.toString)
+        val numberTasks: Seq[Task[Long]] = numberStrings.map(toLong)
+        val sumTask: Task[Long] = ZIO.reduceAll(Task(0L), numberTasks)(_ + _)
+        assertM(sumTask)(equalTo(55L))
       }
     )
 
